0fb3b84c0e77d4382feac86c5df2da5c70f0ed34
edricle1302 <108991150+edricle1302@users.noreply.github.com>
2023-06-17 16:31:54 +0700

    update ui

--- src/components/guest/home/components/AdBanner.jsx
+++ src/components/guest/home/components/AdBanner.jsx
@@ -1,5 +1,8 @@
-import { Carousel, Container } from 'react-bootstrap';
-import Banner from 'assets/images/AdBanner.svg';
+import { Container, Col, Row, Carousel } from 'react-bootstrap';
+import Banner_1 from 'assets/images/AdBanner.svg';
+import Banner_2 from 'assets/images/banner_2.jpg';
+import Banner_3 from 'assets/images/banner_3.jpg';
+
 
 const AdBanner = () => {
   return (
@@ -7,25 +10,37 @@ const AdBanner = () => {
       <Container className='ad-banner mx-6 d-flex'>
         <Carousel>
             <Carousel.Item interval={4000}>
+              <Row>
+                <Col>
                 <img
-                className="d-block w-100"
-                src={Banner}
-                alt="First slide"
+                  className="d-block w-100"
+                  src={Banner_1}
+                  alt="First slide"
                 />
+                </Col>
+              </Row>
             </Carousel.Item>
             <Carousel.Item interval={4000}>
+              <Row>
+                <Col>
                 <img
-                className="d-block w-100"
-                src={Banner}
-                alt="Second slide"
+                  className="d-block w-100"
+                  src={Banner_1}
+                  alt="Middle slide"
                 />
+                </Col>
+              </Row>
             </Carousel.Item>
             <Carousel.Item interval={4000}>
+              <Row>
+                <Col>
                 <img
-                className="d-block w-100"
-                src={Banner}
-                alt="Third slide"
+                  className="d-block w-100"
+                  src={Banner_1}
+                  alt="Last slide"
                 />
+                </Col>
+              </Row>
             </Carousel.Item>
         </Carousel>
       </Container>
