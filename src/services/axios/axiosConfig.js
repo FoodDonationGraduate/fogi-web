66696c20528e6480fbdab7798cec598b76d1ce84
edricle1302 <108991150+edricle1302@users.noreply.github.com>
2023-06-30 14:39:00 +0700

    update axios config

--- src/services/axios/axiosConfig.js
+++ src/services/axios/axiosConfig.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: 'https://bachkhoi.online',
-    headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-    },
     timeout: 20000,
 });
 
