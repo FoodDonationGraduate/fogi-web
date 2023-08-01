20dc116aef7142055f21b8bf16d17a2dfee4895f
edricle1302 <108991150+edricle1302@users.noreply.github.com>
2023-08-01 19:05:58 +0700

    fix eslint

--- src/components/director/home/pages/request/details/RequestDetailsPage.jsx
+++ src/components/director/home/pages/request/details/RequestDetailsPage.jsx
@@ -183,8 +183,8 @@ const RequestDetailsPage = () => {
         case 'finding': newStatus = (targetVolunteer || autoDistributeVolunteer) ? 'finding' : 'receiving'; break;
         case 'receiving': newStatus ='shipping'; break;
         case 'shipping': newStatus = 'success'; break;
-        default: return;
       }
+
       var data = ((request.status === 'pending' || request.status === 'finding') && !autoDistributeVolunteer) ? {
         request_status: newStatus,
         request_id: request.id,
@@ -205,7 +205,6 @@ const RequestDetailsPage = () => {
         case 'accepted': newStatus = 'success'; break;
         case 'receiving': newStatus = (request.delivery_type && request.delivery_type === 'pickup') ? 'success' : 'shipping'; break;
         case 'shipping': newStatus = 'success'; break;
-        default: return;
       }
         data =
       (((request.status === 'pending' && request.delivery_type !== 'pickup' ) ||
